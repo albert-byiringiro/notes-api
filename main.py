fad35e2f537cc300f66dded103c42056fbf536c7
Albert Byiringiro <byiringiroalbert20@gmail.com>
2026-03-22 11:26:25 +0200

    refactor: improve main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routers import note
 
-app = FastAPI(title="Simple Notes API")
+app = FastAPI(title="Notes API", version="0.1.0")
+
 app.include_router(note.router)
